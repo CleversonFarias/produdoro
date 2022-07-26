7543ff8c7fd3364b326543a921641094a044f95e
Cleverson Lopes Farias <cleverson_sairaf@outlook.com>
2022-07-25 22:16:21 -0300

    Finalizando o mapeamento

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/domain/Tarefa.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/domain/Tarefa.java
@@ -1,4 +1,4 @@
-package dev.wakandaacademy.produdoro.projeto.domain;
+package dev.wakandaacademy.produdoro.tarefa.domain;
 
 import java.util.UUID;
 
@@ -15,12 +15,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-@Document(collection = "Area")
-public class Area {
+@Document(collection = "Tarefa")
+public class Tarefa {
 	@Id
-	private UUID idArea;
+	private UUID idTarefa;
 	private String nome;	
 	private String descricao;
 	@Indexed
 	private UUID idUsuario;
+	@Indexed
+	private UUID idArea;
+	@Indexed
+	private UUID idProjeto;
+	@Builder.Default
+	private StatusTarefa status = StatusTarefa.A_FAZER;
 }
